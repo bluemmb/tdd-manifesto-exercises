25d8b6ee572cb3f20c50d5f5ae7c2051282f18ed
Mohammad Eftekhari <mohammad.eftekhari@parspack.com>
2022-07-19 18:29:10 +0430

    FizzBuzz should return Fizz for multiples of 3

--- FizzBuzz/FizzBuzz.php
+++ FizzBuzz/FizzBuzz.php
@@ -6,6 +6,9 @@ class FizzBuzz
 {
     public function handle(int $number)
     {
+        if ( $number % 3 == 0 )
+            return "Fizz";
+
         return (string)$number;
     }
 }
--- FizzBuzz/FizzBuzzTest.php
+++ FizzBuzz/FizzBuzzTest.php
@@ -12,4 +12,11 @@ class FizzBuzzTest extends TestCase
         $string = $fizzBuzz->handle(22);
         $this->assertSame("22", $string);
     }
+
+    public function test_fizzbuzz_returns_fizz_for_multiple_of_three()
+    {
+        $fizzBuzz = new FizzBuzz();
+        $string = $fizzBuzz->handle(6);
+        $this->assertSame("Fizz", $string);
+    }
 }
