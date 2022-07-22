46ed98a470b6e034be50759049925d28087780c0
Mohammad Eftekhari <bluemmb22@gmail.com>
2022-07-22 17:36:17 +0430

    Add Scanning namespace

---
+++ Scanning/Scanning.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Scanning;
+
+class Scanning
+{
+
+}
---
+++ Scanning/ScanningTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Scanning;
+
+use PHPUnit\Framework\TestCase;
+
+class ScanningTest extends TestCase
+{
+    
+}
