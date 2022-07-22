e43bcd6d6d0ab0de38a1b0b0bc9a0076777dada8
Mohammad Eftekhari <bluemmb22@gmail.com>
2022-07-22 11:59:56 +0430

    Add should return 0 for empty string

--- StringCalculator/StringCalculator.php
+++ StringCalculator/StringCalculator.php
@@ -4,5 +4,11 @@ namespace StringCalculator;
 
 class StringCalculator
 {
+    public function Add(string $numbers) : int
+    {
+        if ( $numbers == "" )
+            return 0;
 
+        throw new \Exception();
+    }
 }
--- StringCalculator/StringCalculatorTest.php
+++ StringCalculator/StringCalculatorTest.php
@@ -2,7 +2,20 @@
 
 namespace StringCalculator;
 
-class StringCalculatorTest
-{
+use PHPUnit\Framework\TestCase;
 
+class StringCalculatorTest extends TestCase
+{
+    private StringCalculator $stringCalculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+
+    public function test_add_returns_zero_for_empty_strings()
+    {
+        self::assertSame(0, $this->stringCalculator->Add(""));
+    }
 }
